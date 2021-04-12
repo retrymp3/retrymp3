f2a77acc42da775b1b044d84b657231431a2a89e
retrymp3 <65720815+retrymp3@users.noreply.github.com>
2021-04-12 16:36:58 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,30 +1,30 @@
 <?php
 
-function check_login($con)
+function check_login($con) /* function to check if the user is logged in */
 {
 
-	if(isset($_SESSION['user_id']))
+	if(isset($_SESSION['user_id'])) /* checks if session value is set as 'user_id' */
 	{
 
-		$id = $_SESSION['user_id'];
-		$query = "select * from signup where user_id = '$id' limit 1";
+		$id = $_SESSION['user_id']; /* assigns the session value to $id */
+		$query = "select * from signup where user_id = '$id' limit 1"; /* checking the database. limit 1 will return the first record.*/
 
-		$result = mysqli_query($con,$query);
-		if($result && mysqli_num_rows($result) > 0)
+		$result = mysqli_query($con,$query); /* performs the query against the database */
+		if($result && mysqli_num_rows($result) > 0) /* checks to see if the result is positive and there is data present inthe database by returning the number of rows */
 		{
 
-			$user_data = mysqli_fetch_assoc($result);
+			$user_data = mysqli_fetch_assoc($result); /* fetches a result row as an associative array. */
 			return $user_data;
 		}
 	}
 
-	//redirect to login
+	//redirect to login page
 	header("Location: login.php");
 	die;
 
 }
 
-function random_num($length)
+function random_num($length) /* function for generating a random number */
 {
 
 	$text = "";
@@ -36,7 +36,7 @@ function random_num($length)
 	$len = rand(4,$length);
 
 	for ($i=0; $i < $len; $i++) { 
-		# code...
+
 
 		$text .= rand(0,9);
 	}
