3825e6d4bb3b646be687920425bd6d8ef63b8885
retrymp3 <65720815+retrymp3@users.noreply.github.com>
2021-04-12 16:37:20 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,12 +1,12 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "auth";
+$dbhost = "localhost"; /* host name */
+$dbuser = "root"; /* username */
+$dbpass = ""; /* password(default is '') */
+$dbname = "auth"; /* database name */
 
-if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname)) /* checks to see if it doesn't connect to the database */
 {
 
-	die("failed to connect!");
+	die("failed to connect!"); /* error message */
 }
