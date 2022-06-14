278c58040e38a920b64ba5c093facd88afa508c7
Achuth V P <65720815+retrymp3@users.noreply.github.com>
2022-06-14 17:55:42 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -8,5 +8,5 @@ $dbname = "auth"; /* database name */
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname)) /* checks to see if it doesn't connect to the database */
 {
 
-	die("failed to connect!"); /* error message */
+	die("failed to connect!"); /* error message and exits script*/
 }
